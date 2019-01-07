d227413a59f85b48412e0c858ed86e7889b9699d
sarath <sarath@cabin-holiday.com>
2019-01-07 11:53:18 +0100

    Laraspace template removed and added bootstrap 4

--- resources/views/dashboard/layouts/app.blade.php
+++ resources/views/dashboard/layouts/app.blade.php
@@ -2,132 +2,141 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="msapplication-TileImage" content="{{ asset('storage/img/favicons/mstile-144x144.png') }}">
-    <meta name="msapplication-config" content="{{ asset('storage/img/favicons/browserconfig.xml') }}">
-    <meta name="theme-color" content="#333333">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Template script -->
-    <script src="{{ asset('js/pace.js') }}" defer></script>
-
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
-    {{--<link href='https://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'>--}}
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <!-- START CORE CSS-->
-    <link href="{{ asset('css/laraspace.css') }}" rel="stylesheet">
-
-    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('storage/img/favicons/apple-touch-icon-57x57.png') }}">
-    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('storage/img/favicons/apple-touch-icon-60x60.png') }}">
-    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('storage/img/favicons/apple-touch-icon-72x72.png') }}">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('storage/img/favicons/apple-touch-icon-76x76.png') }}">
-    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('storage/img/favicons/apple-touch-icon-114x114.png') }}">
-    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('storage/img/favicons/apple-touch-icon-120x120.png') }}">
-    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('storage/img/favicons/apple-touch-icon-144x144.png') }}">
-    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('storage/img/favicons/apple-touch-icon-152x152.png') }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('storage/img/favicons/apple-touch-icon-180x180.png') }}">
-    <link rel="icon" type="image/png" href="{{ asset('storage/img/favicons/favicon-32x32.png') }}" sizes="32x32">
-    <link rel="icon" type="image/png" href="{{ asset('storage/img/favicons/android-chrome-192x192.png') }}" sizes="192x192">
-    <link rel="icon" type="image/png" href="{{ asset('storage/img/favicons/favicon-96x96.png') }}" sizes="96x96">
-    <link rel="icon" type="image/png" href="{{ asset('storage/img/favicons/favicon-16x16.png') }}" sizes="16x16">
-    <link rel="manifest" href="{{ asset('storage/img/favicons/manifest.json') }}">
-    <link rel="mask-icon" href="{{ asset('storage/img/favicons/safari-pinned-tab.svg') }}" color="#333333">
-    <link rel="shortcut icon" href="{{ asset('storage/img/favicons/favicon.ico') }}">
+    <link href="{{ asset('css/all.css') }}" rel="stylesheet">
 </head>
 
-<body class="layout-default skin-default">
+<body>
 
 <div id="app" class="site-wrapper">
-    <header class="site-header">
-        <a href="#" class="brand-main">
-            <img src="{{ asset('storage/img/logo-desk.png') }}" id="logo-desk" alt="Logo" class="hidden-sm-down">
-            <img src="{{ asset('storage/img/logo-mobile.png') }}" id="logo-mobile" alt="Logo"
-                 class="hidden-md-up">
-        </a>
-        <a href="#" class="nav-toggle">
-            <div class="hamburger hamburger--htla">
-                <span>toggle menu</span>
-            </div>
-        </a>
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark navbar-right">
+        <a class="" href="#">SEO Finder</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-        <ul class="action-list">
-            <li>
-                <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i
-                        class="icon-fa icon-fa-plus"></i></a>
-                <div class="dropdown-menu dropdown-menu-right">
-                    <a class="dropdown-item" href="#"><i class="icon-fa icon-fa-edit"></i> New Post</a>
-                    <a class="dropdown-item" href="#"><i class="icon-fa icon-fa-tag"></i> New Category</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#"><i class="icon-fa icon-fa-star"></i> Separated link</a>
-                </div>
-            </li>
-            <li>
-                <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i
-                        class="icon-fa icon-fa-bell"></i></a>
-                <div class="dropdown-menu dropdown-menu-right notification-dropdown">
-                    <h6 class="dropdown-header">Notifications</h6>
-                    <a class="dropdown-item" href="#"><i class="icon-fa icon-fa-user"></i> New User was Registered</a>
-                    <a class="dropdown-item" href="#"><i class="icon-fa icon-fa-comment"></i> A Comment has been posted.</a>
-                </div>
-            </li>
-            <li>
-                <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="avatar"><img src="{{ asset('storage/img/avatars/avatar.png') }}" alt="Avatar"></a>
-                <div class="dropdown-menu dropdown-menu-right notification-dropdown">
-                    <a class="dropdown-item" href=""><i class="icon-fa icon-fa-cogs"></i> Settings</a>
-                    <a class="dropdown-item" href=""><i class="icon-fa icon-fa-sign-out"></i> Logout</a>
-                </div>
-            </li>
-        </ul>
-    </header>
-    <div class="mobile-menu-overlay"></div>
-    <div class="sidebar-left " id="mobile-nav">
-        <div class="sidebar-body scroll-pane">
-            <ul class="side-nav metismenu" id="menu">
-                <li class="active">
-                    <a href="#"><i class="icon-fa icon-fa-dashboard"></i> Dashboard <span
-                            class="icon-fa arrow icon-fa-fw"></span> </a>
-                    <ul aria-expanded="true" class="collapse">
-                        <li class="active">
-                            <a href="">Basic</a>
-                        </li>
-                        <li class="">
-                            <a href="">Ecommerce</a>
-                        </li>
-                        <li class="">
-                            <a href="">Finance</a>
-                        </li>
-                    </ul>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown active">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Settings <span class="sr-only">(current)</span>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Logout</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" tabindex="-1">Help</a>
                 </li>
             </ul>
+
+        </div>
+    </nav>
+
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+                <div class="sidebar-sticky">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">
+                                <span data-feather="home"></span>
+                                Dashboard <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        {{--<li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span data-feather="file"></span>
+                                Orders
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span data-feather="shopping-cart"></span>
+                                Products
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span data-feather="users"></span>
+                                Customers
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span data-feather="bar-chart-2"></span>
+                                Reports
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span data-feather="layers"></span>
+                                Integrations
+                            </a>
+                        </li>--}}
+                    </ul>
+
+                    {{--<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                        <span>Saved reports</span>
+                        <a class="d-flex align-items-center text-muted" href="#">
+                            <span data-feather="plus-circle"></span>
+                        </a>
+                    </h6>
+                    <ul class="nav flex-column mb-2">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span data-feather="file-text"></span>
+                                Current month
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span data-feather="file-text"></span>
+                                Last quarter
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span data-feather="file-text"></span>
+                                Social engagement
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span data-feather="file-text"></span>
+                                Year-end sale
+                            </a>
+                        </li>
+                    </ul>--}}
+                </div>
+            </nav>
+
+            @yield('content')
+
         </div>
     </div>
-
-    @yield('content')
-
-    <footer class="site-footer">
-        <div class="text-right">
-            Powered by <a href="http://bytefury.com/" target="_blank">Finder</a>
-        </div>
-    </footer>
 </div>
 
-<!-- Plugins -->
-<script src="{{ asset('js/plugins.js') }}"></script>
-
+<!-- Scripts -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
+<script src="{{ asset('js/all.js') }}"></script>
 </body>
 
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,78 +12,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .js('resources/js/pace.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
 
-mix.styles('resources/css/laraspace.css', 'public/css/laraspace.css');
+mix.styles('resources/css/dashboard.css', 'public/css/all.css');
 
-mix.combine([
-    // ** Required Plugins **
-    'resources/js/plugins/bootstrap/tether.js',
-    'resources/js/plugins/customScrollBar/customScrollBar.js',
-
-    // ** Additional Plugins **
-    'resources/js/plugins/ladda/spin.js',
-    'resources/js/plugins/ladda/ladda.js',
-    'resources/js/plugins/toastr/toastr.js',
-    'resources/js/plugins/notie/notie.js',
-    'resources/js/plugins/jquery-validate/jquery.validate.js',
-    'resources/js/plugins/jquery-validate/additional-methods.js',
-    'resources/js/plugins/clockpicker/bootstrap-clockpicker.js',
-    'resources/js/plugins/switchery/switchery.js',
-    'resources/js/plugins/select2/select2.js',
-    'resources/js/plugins/datatables/dataTables.min.js',
-    'resources/js/plugins/datatables/dataTables.bootstrap.js',
-    'resources/js/plugins/multiselect/jquery.multi-select.js',
-    'resources/js/plugins/bootstrapSelect/bootstrap-select.js',
-    'resources/js/plugins/bootstrap-datepicker/bootstrap-datepicker.js',
-    'resources/js/plugins/timepicker/jquery.timepicker.js',
-    'resources/js/plugins/summernote/summernote.js',
-    'resources/js/plugins/simplemde/simplemde.min.js',
-    'resources/js/plugins/prism/prism.js',
-    'resources/js/plugins/charts/sparkline/jquery.sparkline.js',
-    'resources/js/plugins/charts/amcharts/amcharts.js',
-    'resources/js/plugins/charts/amcharts/serial.js',
-    'resources/js/plugins/charts/amcharts/amstock.js',
-    'resources/js/plugins/charts/amcharts/xy.js',
-    'resources/js/plugins/charts/amcharts/ammap.js',
-    'resources/js/plugins/charts/amcharts/worldLow.js',
-    'resources/js/plugins/charts/amcharts/pie.js',
-    'resources/js/plugins/charts/amcharts/animate.js',
-    'resources/js/plugins/charts/amcharts/export.js',
-    'resources/js/plugins/charts/amcharts/radar.js',
-    'resources/js/plugins/charts/amcharts/polarScatter.js',
-    'resources/js/plugins/charts/amcharts/light.js',
-    'resources/js/plugins/charts/morris/raphael.js',
-    'resources/js/plugins/charts/morris/morris.js',
-    'resources/js/plugins/charts/gauges/gauge.js',
-    'resources/js/plugins/alertify/alertify.js',
-    'resources/js/plugins/easypiecharts/jquery.easypiechart.js',
-    'resources/js/plugins/metisMenu/metisMenu.js',
-    'resources/js/plugins/form-wizard/jquery.steps.js',
-    'resources/js/plugins/bar-rating/jquery.barrating.js',
-    'resources/js/plugins/nestable/nestable.js',
-    'resources/js/plugins/jstree/jstree.js',
-    'resources/js/plugins/gallery/image/photoswipe.js',
-    'resources/js/plugins/gallery/image/photoswipe-ui-default.js',
-    'resources/js/plugins/gallery/image/mp.mansory.js',
-    'resources/js/plugins/gallery/video/videobox.js',
-    'resources/js/plugins/rating/jquery.raty.js',
-    'resources/js/plugins/zoom/transition.js',
-    'resources/js/plugins/zoom/zoom.js',
-    'resources/js/plugins/calendar/moment.js',
-    'resources/js/plugins/calendar/fullcalendar.js',
-    'resources/js/plugins/imageCropper/cropper.js',
-    'resources/js/plugins/ace-editor/ace.js',
-    'resources/js/plugins/icons/evil.js',
-    'resources/js/plugins/ace-editor/twilight.js',
-
-    // ** Laraspace Inits **
-    'resources/js/plugins/laraspace/laraspace-layout.js',
-    'resources/js/plugins/laraspace/laraspace-notifs.js',
-    'resources/js/plugins/laraspace/laraspace-forms.js'
-
-], 'public/js/plugins.js');
+/*mix.scripts('resources/js/dashboard.js', 'public/js/all.js');*/
 
 if (mix.inProduction()) {
     mix.version();
